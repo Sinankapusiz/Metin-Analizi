fd56b01d6794ede2191deb692b86dac905c9df6b
Sinan Kapusız <sinankapusiz@outlook.com>
2025-03-25 23:16:39 +0300

    güncelleme 1.0.1

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const alfabe = "abcçdefgğhıijklmnoöprsştuüvyzxw";
 const rakam = "0123456789";
 const sembol = ".,;:!+-×÷=≈√∞$€₺£¥←→↑↓↔©®™★♥♠♣";
-
+let kelimeDizisi = [];
+let secilenElement = null;
+let analizDurumu = false;
 
 const metin = document.getElementById('metinBox');
 const analizBtn = document.getElementById('analizBtn');
+const yeniAnalizBtn = document.getElementById('yeni-analizBtn');
 const alfabeTable = document.getElementById('alfabe-table');
 const rakamTable = document.getElementById('rakam-table');
 const sembolTable = document.getElementById('sembol-table');
 const ozetTable = document.getElementById('ozet-table');
+const favoriKelimeler = document.getElementById('favori-kelimeler');
+const container = document.querySelector(".metin-girisi-panel");
+
+const metinBox2 = document.createElement("div");
 
 function kelimeSay(gonderilenMetin) {
-    return gonderilenMetin.trim().split(" ").length - 1;
+    kelimeDizisi = gonderilenMetin.trim().split(/\s+/).map(kelime => kelime.replace(/[^a-zA-ZğüşöçıİĞÜŞÖÇ]+/g, '')).filter(kelime => kelime.length > 0);
+    return kelimeDizisi.length;
 };
 
 function harfSay(gonderilenMetin, gonderilenHarf) {
@@ -34,7 +42,29 @@ function boslukSay(gonderilenMetin) {
     return gonderilenMetin.split(" ").length - 1;
 };
 
+function favoriKelimeleriTespitEt(metinDizisi) {
+    let favoriKelimeDizisi = [];
+    for (let i = 0; i < metinDizisi.length; i++) {
+        let favoriKelimeSayaci = 0;
 
+        for (let j = 0; j < metinDizisi.length; j++) {
+            if (metinDizisi[i] === metinDizisi[j])
+                favoriKelimeSayaci++;
+        }
+        if (favoriKelimeSayaci > 2 && !favoriKelimeDizisi.includes(metinDizisi[i])) {
+            favoriKelimeDizisi.push(metinDizisi[i]);
+            favoriKelimeDizisi.push(favoriKelimeSayaci);
+        }
+    }
+    return favoriKelimeDizisi;
+};
+
+function favoriKelimeleriDivEkle() {
+    let favoriKelimeDizisi = favoriKelimeleriTespitEt(kelimeDizisi);
+    for (let i = 0; i < favoriKelimeDizisi.length; i += 2) {
+        favoriKelimeler.innerHTML += `<span class="favori-kelime">${favoriKelimeDizisi[i]} (${favoriKelimeDizisi[i + 1]})</span>`;
+    }
+}
 
 function metniAnalizEt(metin) {
 
@@ -83,10 +113,109 @@ function metniAnalizEt(metin) {
     // Boşluk Sayısı
     ozetTable.rows[0].cells[9].innerHTML = boslukSay(metin);
 
+    favoriKelimeleriDivEkle();
+
+    favoriKelimeleriVurgula();
+    harfleriVurgula();
+    rakamlariVurgula();
+    sembolleriVurgula();
 };
 
+function istenilenKelimeyiVurgula(kelime) {
+    metinBox2.innerHTML = metin.value;
+    const regex = new RegExp(`${kelime}`, 'gi');
+    const yeniMetin = metinBox2.innerHTML.replace(regex, `<span class="highlight">${kelime}</span>`);
 
+    metinBox2.innerHTML = yeniMetin.replace(/\n/g, '<br>');
+}
 analizBtn.addEventListener("click", () => {
+    analizDurumu = true;
     metniAnalizEt(metin.value);
     console.log("Analiz Bitti");
+
+    analizBtn.style.display = "none";
+    yeniAnalizBtn.style.display = "block";
+    metin.style.display = "none";
+    metinBox2.classList.add("metinBox");
+    metinBox2.innerHTML = metin.value;
+    container.appendChild(metinBox2);
+    metinBox2.style.display = "block";
+    if (secilenElement !== null) {
+        secilenElement.classList.remove("secilmis-element");
+        secilenElement = null;
+    }
+
 });
+yeniAnalizBtn.addEventListener("click", () => {
+    yeniAnalizBtn.style.display = "none";
+    analizBtn.style.display = "block";
+    metin.style.display = "block";
+    metinBox2.style.display = "none";
+    metin.value = "";
+    metinBox2.innerHTML = "";
+    const sncHucre = document.querySelectorAll(".sncH");
+    sncHucre.forEach(hucre => {
+        hucre.textContent = "0";
+    })
+    if (secilenElement !== null) {
+        secilenElement.classList.remove("secilmis-element");
+        secilenElement = null;
+    }
+    favoriKelimeler.innerHTML = "";
+    analizDurumu = false;
+});
+
+function favoriKelimeleriVurgula() {
+    const allSpanFvr = document.querySelectorAll(".favori-kelime");
+    allSpanFvr.forEach(btn => {
+        btn.addEventListener("click", () => {
+            const btnMetin = btn.textContent.split(" ");
+            istenilenKelimeyiVurgula(btnMetin[0]);
+            secimiDegistir(btn);
+        })
+    })
+}
+function harfleriVurgula() {
+    const allHarf = document.querySelectorAll(".harf");
+
+    allHarf.forEach(btn => {
+        btn.onclick = function () {
+            if (analizDurumu === true) {
+                istenilenKelimeyiVurgula(btn.textContent[0]);
+                secimiDegistir(btn);
+            }
+        };
+    })
+};
+function rakamlariVurgula() {
+    const allrakam = document.querySelectorAll(".rakam");
+
+    allrakam.forEach(btn => {
+        btn.onclick = function () {
+            if (analizDurumu === true) {
+                istenilenKelimeyiVurgula(btn.textContent[0]);
+                secimiDegistir(btn);
+            }
+        };
+    })
+};
+function sembolleriVurgula() {
+    const allsembol = document.querySelectorAll(".sembol");
+
+    allsembol.forEach(btn => {
+        btn.onclick = function () {
+            if (analizDurumu === true) {
+                istenilenKelimeyiVurgula(btn.textContent[0]);
+                secimiDegistir(btn);
+            }
+        };
+    })
+};
+
+function secimiDegistir(degisenElement) {
+    degisenElement.classList.add("secilmis-element");
+    if (secilenElement !== null) {
+        secilenElement.classList.remove("secilmis-element");
+    }
+    secilenElement = degisenElement;
+};
